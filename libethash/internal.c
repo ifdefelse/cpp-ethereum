67cf8a35b8ac6a4e413a292c888fa5e70d633e43
ifdefelse <ifdefelse@protonmail.com>
2018-05-18 17:56:26 +0800

    Add the ProgPow code for client

--- libethash/internal.c
+++ libethash/internal.c
@@ -182,6 +182,304 @@ bool ethash_compute_full_data(
 	return true;
 }
 
+//***************************************************************
+//***************************************************************
+typedef struct
+{
+    uint32_t uint32s[32 / sizeof(uint32_t)];
+} hash32_t;
+
+#define PROGPOW_LANES                   32
+#define PROGPOW_REGS                    16
+#define PROGPOW_CACHE_BYTES             (16*1024)
+#define PROGPOW_CNT_MEM                 ETHASH_ACCESSES
+#define PROGPOW_CNT_CACHE               8
+#define PROGPOW_CNT_MATH                8
+#define PROGPOW_CACHE_WORDS  (PROGPOW_CACHE_BYTES / sizeof(uint32_t))
+
+//#define ROTL32(x,n) __funnelshift_l((x), (x), (n))
+//#define ROTR32(x,n) __funnelshift_r((x), (x), (n))
+#define ROTL(x,n,w) (((x) << (n)) | ((x) >> ((w) - (n))))
+#define ROTL32(x,n) ROTL(x,n,32)	/* 32 bits word */
+
+#define ROTR(x,n,w) (((x) >> (n)) | ((x) << ((w) - (n))))
+#define ROTR32(x,n) ROTR(x,n,32)	/* 32 bits word */
+//#define ROTR32(x, n)  (((0U + (x)) << (32 - (n))) | ((x) >> (n)))  // Assumes that x is uint32_t and 0 < n < 32
+
+
+#define min(a,b) ((a<b) ? a : b)
+//#define mul_hi(a, b) __umulhi(a, b)
+uint32_t mul_hi (uint32_t a, uint32_t b){
+    uint64_t result = (uint64_t) a * (uint64_t) b;
+    return  (uint32_t) (result>>32);
+}
+//#define clz(a) __clz(a)
+uint32_t clz (uint32_t a){
+    uint32_t result = 0;
+    for(int i=31;i>=0;i--){
+        if(((a>>i)&1) == 1)
+            result ++;
+        else
+            break;
+    }
+    return result;
+}
+//#define popcount(a) __popc(a)
+uint32_t popcount (uint32_t a){
+    uint32_t result = 0;
+    for(int i=31;i>=0;i--){
+        if(((a>>i)&1) == 1)
+            result ++;
+    }
+    return result;
+}
+
+void swap(int *a, int *b)
+{
+    int t = *a;
+    *a = *b;
+    *b = t;
+}
+
+
+uint32_t fnv1a(uint32_t *h, uint32_t d)
+{
+        return *h = (*h ^ d) * 0x1000193;
+}
+
+
+// Implementation based on:
+// https://github.com/mjosaarinen/tiny_sha3/blob/master/sha3.c
+// converted from 64->32 bit words
+const uint32_t keccakf_rndc[24] = {
+    0x00000001, 0x00008082, 0x0000808a, 0x80008000, 0x0000808b, 0x80000001,
+    0x80008081, 0x00008009, 0x0000008a, 0x00000088, 0x80008009, 0x8000000a,
+    0x8000808b, 0x0000008b, 0x00008089, 0x00008003, 0x00008002, 0x00000080,
+    0x0000800a, 0x8000000a, 0x80008081, 0x00008080, 0x80000001, 0x80008008
+};
+
+ void keccak_f800_round(uint32_t st[25], const int r)
+{
+
+    const uint32_t keccakf_rotc[24] = {
+        1,  3,  6,  10, 15, 21, 28, 36, 45, 55, 2,  14,
+        27, 41, 56, 8,  25, 43, 62, 18, 39, 61, 20, 44
+    };
+    const uint32_t keccakf_piln[24] = {
+        10, 7,  11, 17, 18, 3, 5,  16, 8,  21, 24, 4,
+        15, 23, 19, 13, 12, 2, 20, 14, 22, 9,  6,  1
+    };
+
+    uint32_t t, bc[5];
+    // Theta
+    for (int i = 0; i < 5; i++)
+        bc[i] = st[i] ^ st[i + 5] ^ st[i + 10] ^ st[i + 15] ^ st[i + 20];
+
+    for (int i = 0; i < 5; i++) {
+        t = bc[(i + 4) % 5] ^ ROTL32(bc[(i + 1) % 5], 1);
+        for (uint32_t j = 0; j < 25; j += 5)
+            st[j + i] ^= t;
+    }
+
+    // Rho Pi
+    t = st[1];
+    for (int i = 0; i < 24; i++) {
+        uint32_t j = keccakf_piln[i];
+        bc[0] = st[j];
+        st[j] = ROTL32(t, keccakf_rotc[i]);
+        t = bc[0];
+    }
+
+    //  Chi
+    for (uint32_t j = 0; j < 25; j += 5) {
+        for (int i = 0; i < 5; i++)
+            bc[i] = st[j + i];
+        for (int i = 0; i < 5; i++)
+            st[j + i] ^= (~bc[(i + 1) % 5]) & bc[(i + 2) % 5];
+    }
+
+    //  Iota
+    st[0] ^= keccakf_rndc[r];
+}
+
+ uint64_t keccak_f800(hash32_t header, uint64_t seed, uint32_t *result)
+{
+    uint32_t st[25];
+
+    for (int i = 0; i < 25; i++)
+        st[i] = 0;
+    for (int i = 0; i < 8; i++)
+        st[i] = header.uint32s[i];
+    st[8] = seed;
+    st[9] = seed >> 32;
+    st[10] = result[0];
+    st[11] = result[1];
+    st[12] = result[2];
+    st[13] = result[3];
+
+    for (int r = 0; r < 21; r++) {
+        keccak_f800_round(st, r);
+    }
+    // last round can be simplified due to partial output
+    keccak_f800_round(st, 21);
+
+    return (uint64_t)st[1] << 32 | st[0];
+}
+
+
+typedef struct {
+    uint32_t z, w, jsr, jcong;
+} kiss99_t;
+
+// KISS99 is simple, fast, and passes the TestU01 suite
+// https://en.wikipedia.org/wiki/KISS_(algorithm)
+// http://www.cse.yorku.ca/~oz/marsaglia-rng.html
+uint32_t kiss99(kiss99_t * st)
+{
+    uint32_t znew = (st->z = 36969 * (st->z & 65535) + (st->z >> 16));
+    uint32_t wnew = (st->w = 18000 * (st->w & 65535) + (st->w >> 16));
+    uint32_t MWC = ((znew << 16) + wnew);
+    uint32_t SHR3 = (st->jsr ^= (st->jsr << 17), st->jsr ^= (st->jsr >> 13), st->jsr ^= (st->jsr << 5));
+    uint32_t CONG = (st->jcong = 69069 * st->jcong + 1234567);
+    return ((MWC^CONG) + SHR3);
+}
+
+
+void fill_mix(
+    uint64_t seed,
+    uint32_t lane_id,
+    uint32_t mix[PROGPOW_REGS]
+)
+{
+    // Use FNV to expand the per-warp seed to per-lane
+    // Use KISS to expand the per-lane seed to fill mix
+    uint32_t fnv_hash = 0x811c9dc5;
+    kiss99_t st;
+    st.z = fnv1a(&fnv_hash, seed);
+    st.w = fnv1a(&fnv_hash, seed >> 32);
+    st.jsr = fnv1a(&fnv_hash, lane_id);
+    st.jcong = fnv1a(&fnv_hash, lane_id);
+    for (int i = 0; i < PROGPOW_REGS; i++)
+            mix[i] = kiss99(&st);
+}
+
+kiss99_t progPowInit(uint64_t prog_seed, int mix_seq[PROGPOW_REGS])
+{
+    kiss99_t prog_rnd;
+    uint32_t fnv_hash = 0x811c9dc5;
+    prog_rnd.z = fnv1a(&fnv_hash, prog_seed);
+    prog_rnd.w = fnv1a(&fnv_hash, prog_seed >> 32);
+    prog_rnd.jsr = fnv1a(&fnv_hash, prog_seed);
+    prog_rnd.jcong = fnv1a(&fnv_hash, prog_seed >> 32);
+    // Create a random sequence of mix destinations for merge()
+    // guaranteeing every location is touched once
+    // Uses Fisher–Yates shuffle
+    for (int i = 0; i < PROGPOW_REGS; i++)
+        mix_seq[i] = i;
+    for (int i = PROGPOW_REGS - 1; i > 0; i--)
+    {
+        int j = kiss99(&prog_rnd) % (i + 1);
+        swap(&(mix_seq[i]), &(mix_seq[j]));
+    }
+    return prog_rnd;
+}
+
+// Merge new data from b into the value in a
+// Assuming A has high entropy only do ops that retain entropy
+// even if B is low entropy
+// (IE don't do A&B)
+void merge(uint32_t *a, uint32_t b, uint32_t r)
+{
+    switch (r % 4)
+    {
+    case 0: *a = (*a * 33) + b; break;
+    case 1: *a = (*a ^ b) * 33; break;
+    case 2: *a = ROTL32(*a, ((r >> 16) % 32)) ^ b; break;
+    case 3: *a = ROTR32(*a, ((r >> 16) % 32)) ^ b; break;
+    }
+}
+
+// Random math between two input values
+uint32_t math(uint32_t a, uint32_t b, uint32_t r)
+{
+    switch (r % 11)
+    {
+    case 0: return a + b; break;
+    case 1: return a * b; break;
+    case 2: return mul_hi(a, b); break;
+    case 3: return min(a, b); break;
+    case 4: return ROTL32(a, b); break;
+    case 5: return ROTR32(a, b); break;
+    case 6: return a & b; break;
+    case 7: return a | b; break;
+    case 8: return a ^ b; break;
+    case 9: return clz(a) + clz(b); break;
+    case 10: return popcount(a) + popcount(b); break;
+    default: return 0;
+    }
+    return 0;
+}
+
+// Helper to get the next value in the per-program random sequence
+#define rnd()    (kiss99(&prog_rnd))
+// Helper to pick a random mix location
+#define mix_src() (rnd() % PROGPOW_REGS)
+// Helper to access the sequence of mix destinations
+#define mix_dst() (mix_seq[(mix_seq_cnt++)%PROGPOW_REGS])
+
+void progPowLoop(
+    const uint64_t prog_seed,
+    const uint32_t loop,
+    uint32_t mix[PROGPOW_LANES][PROGPOW_REGS],
+    const uint64_t *g_dag,
+    const uint32_t *c_dag,
+    const uint32_t progpow_dag_words)
+{
+    // All lanes share a base address for the global load
+    // Global offset uses mix[0] to guarantee it depends on the load result
+    uint32_t offset_g = mix[loop%PROGPOW_LANES][0] % progpow_dag_words;
+    // Lanes can execute in parallel and will be convergent
+    for (int l = 0; l < PROGPOW_LANES; l++)
+    {
+        // global load to sequential locations
+        uint64_t data64 = g_dag[offset_g + l];
+
+        // initialize the seed and mix destination sequence
+        int mix_seq[PROGPOW_REGS];
+        int mix_seq_cnt = 0;
+        kiss99_t prog_rnd = progPowInit(prog_seed, mix_seq);
+
+        uint32_t offset, data32;
+        //int max_i = max(PROGPOW_CNT_CACHE, PROGPOW_CNT_MATH);
+        int max_i;
+        if(PROGPOW_CNT_CACHE > PROGPOW_CNT_MATH)
+          max_i = PROGPOW_CNT_CACHE;
+        else
+          max_i = PROGPOW_CNT_MATH;
+        for (int i = 0; i < max_i; i++)
+        {
+            if (i < PROGPOW_CNT_CACHE)
+            {
+                // Cached memory access
+                // lanes access random location
+                offset = mix[l][mix_src()] % PROGPOW_CACHE_WORDS;
+                data32 = c_dag[offset];
+                merge(&(mix[l][mix_dst()]), data32, rnd());
+            }
+            if (i < PROGPOW_CNT_MATH)
+            {
+                // Random Math
+                data32 = math(mix[l][mix_src()], mix[l][mix_src()], rnd());
+                merge(&(mix[l][mix_dst()]), data32, rnd());
+            }
+        }
+        // Consume the global load data at the very end of the loop
+        // Allows full latency hiding
+        merge(&(mix[l][0]), data64, rnd());
+        merge(&(mix[l][mix_dst()]), data64>>32, rnd());
+    }
+}
+
 static bool ethash_hash(
 	ethash_return_value_t* ret,
 	node const* full_nodes,
@@ -191,107 +489,125 @@ static bool ethash_hash(
 	uint64_t const nonce
 )
 {
-	if (full_size % MIX_WORDS != 0) {
-		return false;
-	}
+    
+    const uint64_t *g_dag = (uint64_t *) full_nodes;
+   
+    const hash32_t header;
+    memcpy((void *)&header, (void *)&header_hash, sizeof(header_hash));
+    uint32_t c_dag[PROGPOW_CACHE_WORDS];
+    for(int threadIdx = 0; threadIdx<PROGPOW_LANES; threadIdx++)
+    {
+        // Load random data into the cache
+        // TODO: should be a new blob of data, not existing DAG data
+        for (uint32_t word = threadIdx*2; word < PROGPOW_CACHE_WORDS; word += PROGPOW_LANES*2)
+        {
+            uint64_t data = g_dag[word];
+            c_dag[word + 0] = data;
+            c_dag[word + 1] = data >> 32;
+        }
+    }
 
-	// pack hash and nonce together into first 40 bytes of s_mix
-	assert(sizeof(node) * 8 == 512);
-	node s_mix[MIX_NODES + 1];
-	memcpy(s_mix[0].bytes, &header_hash, 32);
-	fix_endian64(s_mix[0].double_words[4], nonce);
+    if(!full_nodes){
+        printf("Error, the client does not support light node at the moment\n");
+        //printf("random output, do not trust! light %d full_size %d\n", (uint32_t) light, (uint32_t)full_size);
+        exit(1);
+    }
 
-	// compute sha3-512 hash and replicate across mix
-	SHA3_512(s_mix->bytes, s_mix->bytes, 40);
-	fix_endian_arr32(s_mix[0].words, 16);
+    uint32_t mix[PROGPOW_LANES][PROGPOW_REGS];
+    uint32_t result[4];
+    for (int i = 0; i < 4; i++)
+        result[i] = 0;
 
-	node* const mix = s_mix + 1;
-	for (uint32_t w = 0; w != MIX_WORDS; ++w) {
-		mix->words[w] = s_mix[0].words[w % NODE_WORDS];
-	}
+    // keccak(header..nonce)
+    uint64_t seed = keccak_f800(header, nonce, result);
 
-	unsigned const page_size = sizeof(uint32_t) * MIX_WORDS;
-	unsigned const num_full_pages = (unsigned) (full_size / page_size);
+    // initialize mix for all lanes
+    for (int l = 0; l < PROGPOW_LANES; l++)
+        fill_mix(seed, l, mix[l]);
 
-	for (unsigned i = 0; i != ETHASH_ACCESSES; ++i) {
-		uint32_t const index = fnv_hash(s_mix->words[0] ^ i, mix->words[i % MIX_WORDS]) % num_full_pages;
+    uint32_t dagWords = (unsigned)(full_size / ETHASH_MIX_BYTES);
+    // execute the randomly generated inner loop
+    for (int i = 0; i < PROGPOW_CNT_MEM; i++)
+        progPowLoop(light->block_number, i, mix, g_dag, c_dag, dagWords);
 
-		for (unsigned n = 0; n != MIX_NODES; ++n) {
-			node const* dag_node;
-			node tmp_node;
-			if (full_nodes) {
-				dag_node = &full_nodes[MIX_NODES * index + n];
-			} else {
-				ethash_calculate_dag_item(&tmp_node, index * MIX_NODES + n, light);
-				dag_node = &tmp_node;
-			}
+    // Reduce mix data to a single per-lane result
+    uint32_t lane_hash[PROGPOW_LANES];
+    for (int l = 0; l < PROGPOW_LANES; l++)
+    {
+        lane_hash[l] = 0x811c9dc5;
+        for (int i = 0; i < PROGPOW_REGS; i++)
+            fnv1a(&lane_hash[l], mix[l][i]);
+    }
+    // Reduce all lanes to a single 128-bit result
+    for (int i = 0; i < 4; i++)
+        result[i] = 0x811c9dc5;
+    for (int l = 0; l < PROGPOW_LANES; l++)
+        fnv1a(&result[l%4], lane_hash[l]);
 
-#if defined(_M_X64) && ENABLE_SSE
-			{
-				__m128i fnv_prime = _mm_set1_epi32(FNV_PRIME);
-				__m128i xmm0 = _mm_mullo_epi32(fnv_prime, mix[n].xmm[0]);
-				__m128i xmm1 = _mm_mullo_epi32(fnv_prime, mix[n].xmm[1]);
-				__m128i xmm2 = _mm_mullo_epi32(fnv_prime, mix[n].xmm[2]);
-				__m128i xmm3 = _mm_mullo_epi32(fnv_prime, mix[n].xmm[3]);
-				mix[n].xmm[0] = _mm_xor_si128(xmm0, dag_node->xmm[0]);
-				mix[n].xmm[1] = _mm_xor_si128(xmm1, dag_node->xmm[1]);
-				mix[n].xmm[2] = _mm_xor_si128(xmm2, dag_node->xmm[2]);
-				mix[n].xmm[3] = _mm_xor_si128(xmm3, dag_node->xmm[3]);
-			}
-			#elif defined(__MIC__)
-			{
-				// __m512i implementation via union
-				//	Each vector register (zmm) can store sixteen 32-bit integer numbers
-				__m512i fnv_prime = _mm512_set1_epi32(FNV_PRIME);
-				__m512i zmm0 = _mm512_mullo_epi32(fnv_prime, mix[n].zmm[0]);
-				mix[n].zmm[0] = _mm512_xor_si512(zmm0, dag_node->zmm[0]);
-			}
-			#else
-			{
-				for (unsigned w = 0; w != NODE_WORDS; ++w) {
-					mix[n].words[w] = fnv_hash(mix[n].words[w], dag_node->words[w]);
-				}
-			}
-#endif
-		}
+    
+    memcpy(&ret->result, result, sizeof(result));
+    memset((char *)&ret->result + sizeof(result), 0, sizeof(ret->result)-sizeof(result));
 
-	}
+    memcpy(&ret->mix_hash, mix, 32);
 
-// Workaround for a GCC regression which causes a bogus -Warray-bounds warning.
-// The regression was introduced in GCC 4.8.4, fixed in GCC 5.0.0 and backported to GCC 4.9.3 but
-// never to the GCC 4.8.x line.
+    //// keccak(header .. keccak(header..nonce) .. result);
+    //return (keccak_f800(header, seed, result) <= target);
+
+
+
+
+//	if (full_size % MIX_WORDS != 0) {
+//		return false;
+//	}
 //
-// See https://gcc.gnu.org/bugzilla/show_bug.cgi?id=56273
+//	// pack hash and nonce together into first 40 bytes of s_mix
+//	assert(sizeof(node) * 8 == 512);
+//	node s_mix[MIX_NODES + 1];
+//	memcpy(s_mix[0].bytes, &header_hash, 32);
+//	fix_endian64(s_mix[0].double_words[4], nonce);
 //
-// This regression is affecting Debian Jesse (8.5) builds of cpp-ethereum (GCC 4.9.2) and also
-// manifests in the doublethinkco armel v5 cross-builds, which use crosstool-ng and resulting
-// in the use of GCC 4.8.4.  The Tizen runtime wants an even older GLIBC version - the one from
-// GCC 4.6.0!
-
-#if defined(__GNUC__) && (__GNUC__ < 5)
-#pragma GCC diagnostic push
-#pragma GCC diagnostic ignored "-Warray-bounds"
-#endif // define (__GNUC__)
-
-	// compress mix
-	for (uint32_t w = 0; w != MIX_WORDS; w += 4) {
-		uint32_t reduction = mix->words[w + 0];
-		reduction = reduction * FNV_PRIME ^ mix->words[w + 1];
-		reduction = reduction * FNV_PRIME ^ mix->words[w + 2];
-		reduction = reduction * FNV_PRIME ^ mix->words[w + 3];
-		mix->words[w / 4] = reduction;
-	}
-
-#if defined(__GNUC__) && (__GNUC__ < 5)
-#pragma GCC diagnostic pop
-#endif // define (__GNUC__)
-
-	fix_endian_arr32(mix->words, MIX_WORDS / 4);
-	memcpy(&ret->mix_hash, mix->bytes, 32);
-	// final Keccak hash
-	SHA3_256(&ret->result, s_mix->bytes, 64 + 32); // Keccak-256(s + compressed_mix)
+//	// compute sha3-512 hash and replicate across mix
+//	SHA3_512(s_mix->bytes, s_mix->bytes, 40);
+//	fix_endian_arr32(s_mix[0].words, 16);
+//
+//	node* const mix = s_mix + 1;
+//	for (uint32_t w = 0; w != MIX_WORDS; ++w) {
+//		mix->words[w] = s_mix[0].words[w % NODE_WORDS];
+//	}
+//
+//	unsigned const page_size = sizeof(uint32_t) * MIX_WORDS;
+//	unsigned const num_full_pages = (unsigned) (full_size / page_size);
+//
+//	for (unsigned i = 0; i != ETHASH_ACCESSES; ++i) {
+//		uint32_t const index = fnv_hash(s_mix->words[0] ^ i, mix->words[i % MIX_WORDS]) % num_full_pages;
+//
+//		for (unsigned n = 0; n != MIX_NODES; ++n) {
+//			node const* dag_node;
+//			node tmp_node;
+//			if (full_nodes) {
+//				dag_node = &full_nodes[MIX_NODES * index + n];
+//			} else {
+//				ethash_calculate_dag_item(&tmp_node, index * MIX_NODES + n, light);
+//				dag_node = &tmp_node;
+//			}
+//
+//			{
+//				for (unsigned w = 0; w != NODE_WORDS; ++w) {
+//					mix[n].words[w] = fnv_hash(mix[n].words[w], dag_node->words[w]);
+//				}
+//			}
+//		}
+//
+//	}
+//
+//	fix_endian_arr32(mix->words, MIX_WORDS / 4);
+//	memcpy(&ret->mix_hash, mix->bytes, 32);
+//	// final Keccak hash
+//	SHA3_256(&ret->result, s_mix->bytes, 64 + 32); // Keccak-256(s + compressed_mix)
 	return true;
 }
+
+
 
 void ethash_quick_hash(
 	ethash_h256_t* return_hash,
